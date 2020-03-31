9969519c275aa4a0a2288bdf715adf833e3f723d
Nisrin Dhoondia <38102371+mygoal-javadeveloper@users.noreply.github.com>
2020-03-31 18:53:11 +0530

    Add files via upload

---
+++ Probability and Statistics/Probability: Fundamentals/Probability Rules-378.py
@@ -0,0 +1,65 @@
+## 1. Sample Space ##
+
+coin_toss_omega = {'HT','HH', 'TH', 'TT'}
+print(coin_toss_omega)
+
+
+## 2. Probability of Events ##
+
+p_sum_6 = 5/36
+print(p_sum_6)
+p_lower_15 = 36/36
+print(p_lower_15)
+p_greater_13 = 0/36
+print(p_greater_13)
+
+## 3. Certain and Impossible Events ##
+
+p_2_or_4 = 4/36
+print(p_2_or_4)
+p_12_or_13= 1/36
+print(p_12_or_13)
+
+## 4. The Addition Rule ##
+
+p_5_or_9 = 8/36
+print(p_5_or_9)
+p_even_or_less_2 = 18/36
+print(p_even_or_less_2)
+p_4_or_3_multiple = 15/36
+print(p_4_or_3_multiple)
+
+## 5. Venn Diagrams ##
+
+p_c =3/6
+print(p_c)
+p_d =3/6
+print(p_d)
+p_c_d_addition = p_c + p_d
+print(p_c_d_addition)
+p_c_d_formula = 4/6
+print(p_c_d_formula)
+
+## 6. Exceptions to the Addition Rule ##
+
+p_f = 0.26
+p_t = 0.11 
+p_tf = 0.03
+p_f_or_t = p_f + p_t - p_tf
+print(p_f_or_t)
+
+## 7. Mutually Exclusive Events ##
+
+p_h_and_c = 0.08 + 0.11 - 0.17
+print(p_h_and_c)
+
+## 8. Set Notation ##
+
+operation_1 = False
+print(operation_1)
+operation_2 = True
+print(operation_2)
+operation_3 = False
+print(operation_3)
+operation_4 = True
+print(operation_4)
